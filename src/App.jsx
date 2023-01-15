1d8b0d43362a654b6c2405e41bb327cd01dfe64a
AlvaroRuCh <alvaroch250@gmail.com>
2023-01-15 20:59:15 +0100

    Fix Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import './App.css'
-import { Route, Switch, useLocation } from 'wouter'
-import SearchPage from './pages/SearchPage'
-import CharacterPage from './pages/CharacterPage'
-import EpisodePage from './pages/EpisodePage'
+import { useLocation } from 'wouter'
 import Logo from './assets/loader-logo.png'
 import SocialNetworks from './components/SocialNetworks'
+import RouteController from 'components/RouteController'
 function App() {
   const [location, pushLocation] = useLocation();
 
@@ -13,11 +11,8 @@ function App() {
       <button onClick={() => {
         pushLocation('/')
       }} className='logo'><img src={Logo} alt="" /> <span>by Aruger.dev</span></button>
-      <Switch>
-        <Route path='/' component={SearchPage}></Route>
-        <Route path='/character/:id' component={CharacterPage}></Route>
-        <Route path='/episode/:id' component={EpisodePage}></Route>
-      </Switch>
+
+      <RouteController></RouteController>
       <SocialNetworks></SocialNetworks>
     </div>
   )
---
+++ src/components/RouteController/index.jsx
@@ -0,0 +1,14 @@
+import { Route, Switch } from 'wouter'
+import SearchPage from './pages/SearchPage'
+import CharacterPage from './pages/CharacterPage'
+import EpisodePage from './pages/EpisodePage'
+
+export default function RouteController() {
+    return (
+        <Switch>
+            <Route path='/' component={SearchPage}></Route>
+            <Route path='/character/:id' component={CharacterPage}></Route>
+            <Route path='/episode/:id' component={EpisodePage}></Route>
+        </Switch>
+    )
+}
